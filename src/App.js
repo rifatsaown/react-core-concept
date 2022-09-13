869dc39cbde2e09944ff8cc65d31ec4c24563f29
rifatsaown <rifatsaown0@gmail.com>
2022-09-13 16:20:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+const name = "Rifat";
+const rifat = {
+  name: "Rifat",
+  age: 23,
+  job: "Web Developer",
+};
+const rifatStyle  = {
+  color: "white",
+  backgroundColor: "salmon",
+  borderRadius: "20px",
+  margin: "20px",
+  padding: "20px",
+  display: "inline-block",
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>JSX</h1>
+      <div className="container">
+        <h3>Hello this is react</h3>
+      </div>
+      <br />
+      <div style={rifatStyle} className="rifat">
+          <p style={{textTransform:"uppercase",fontWeight:"bold"}}>My name is {name}</p>
+          <p>My name is {rifat.name}</p>
+          <p>My age is {rifat.age}</p>
+          <p>My job is {rifat.job}</p>
+        </div>
     </div>
   );
 }
